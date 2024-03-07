0dd5e1cda4333fcb1882309adb73b7c9e9beac85
mcy03 <manuelcaler2003@gmail.com>
2024-03-07 17:12:07 +0100

    categories en

--- database/seeders/CreateCategoryEventSeeder.php
+++ database/seeders/CreateCategoryEventSeeder.php
@@ -17,55 +17,55 @@ class CreateCategoryEventSeeder extends Seeder
     {
         $categories = [
             [
-                'id' => 1,
-                'name' => 'conferencia',
-                'description' => 'Eventos donde se llevan a cabo charlas, paneles de discusión y presentaciones sobre temas específicos, tanto en persona como en línea.'
+                "id"=> 1,
+                "name"=> "conference",
+                "description"=> "Events where talks, panel discussions, and presentations on specific topics take place, both in person and online."
             ],
             [
-                'id' => 2,
-                'name' => 'taller',
-                'description' => 'Sesiones de aprendizaje intensivas centradas en habilidades específicas, como talleres de cocina, seminarios de desarrollo profesional, etc.'
+                "id"=> 2,
+                "name"=> "workshop",
+                "description"=> "Intensive learning sessions focused on specific skills, such as cooking workshops, professional development seminars, etc."
             ],
             [
-                'id' => 3,
-                'name' => 'feria comercial',
-                'description' => 'Eventos donde empresas y organizaciones exhiben sus productos, servicios o ideas, tanto en entornos físicos como virtuales.'
+                "id"=> 3,
+                "name"=> "trade fair",
+                "description"=> "Events where companies and organizations showcase their products, services, or ideas, both in physical and virtual environments."
             ],
             [
-                'id' => 4,
-                'name' => 'espectáculo en vivo',
-                'description' => 'Presentaciones musicales, de teatro, danza u otras formas de entretenimiento en vivo.'
+                "id"=> 4,
+                "name"=> "live performance",
+                "description"=> "Live musical, theatrical, dance, or other forms of entertainment presentations."
             ],
             [
-                'id' => 5,
-                'name' => 'Eventos deportivos',
-                'description' => 'Competiciones deportivas, torneos, carreras y otros eventos relacionados con el deporte.'
+                "id"=> 5,
+                "name"=> "sports event",
+                "description"=> "Sports competitions, tournaments, races, and other sports-related events."
             ],
             [
-                'id' => 6,
-                'name' => 'encuentro profesional',
-                'description' => 'Oportunidades para establecer contactos profesionales, intercambiar ideas y colaborar con otros profesionales en tu campo.'
+                "id"=> 6,
+                "name"=> "professional meeting",
+                "description"=> "Opportunities to network, exchange ideas, and collaborate with other professionals in your field."
             ],
             [
-                'id' => 7,
-                'name' => 'Evento cultural',
-                'description' => 'Exhibiciones de arte, festivales culturales, proyecciones de cine, lecturas de poesía, etc.'
+                "id"=> 7,
+                "name"=> "cultural event",
+                "description"=> "Art exhibitions, cultural festivals, film screenings, poetry readings, etc."
             ],
             [
-                'id' => 8,
-                'name' => 'Evento gastronómico',
-                'description' => 'Degustaciones de vinos, festivales de comida, clases de cocina, tours gastronómicos, etc.'
+                "id"=> 8,
+                "name"=> "gastronomic event",
+                "description"=> "Wine tastings, food festivals, cooking classes, gastronomic tours, etc."
             ],
             [
-                'id' => 9,
-                'name' => 'estilo de vida',
-                'description' => 'Clases de yoga, retiros de meditación, ferias de salud y bienestar, etc.'
+                "id"=> 9,
+                "name"=> "lifestyle event",
+                "description"=> "Yoga classes, meditation retreats, health and wellness fairs, etc."
             ],
             [
-                'id' => 10,
-                'name' => 'evento familiar',
-                'description' => 'Actividades y entretenimiento diseñados para familias y niños, como espectáculos de magia, días de campo, etc.'
-            ],
+                "id"=> 10,
+                "name"=> "family event",
+                "description"=> "Activities and entertainment designed for families and children, such as magic shows, picnics, etc."
+            ]
         ];
 
         foreach ($categories as $category) {
